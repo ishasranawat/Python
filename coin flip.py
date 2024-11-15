c23362919434377054964f7f65a0a17f9e4f8135
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-15 23:08:57 +0530

    Update coin flip.py

--- coin flip.py
+++ coin flip.py
@@ -4,3 +4,18 @@ if x==1:
  print("Heads")
 else:
  print("Tails")
+
+
+# identify spam comment 
+
+a="click here"
+b="buy now"
+c="subscribe"
+
+message= input("Enter your phrase: ")
+
+if(a in message or b in message or c in message):
+  print("spam")
+
+else:
+  print("not a spam")
