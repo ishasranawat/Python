658b94a5649e96e7312b8e24c9505bcdfc4255ac
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-17 10:49:27 +0530

    Update grade_finder.py

--- grade_finder.py
+++ grade_finder.py
@@ -1,3 +1,4 @@
+# 1.
 a= int(input("Enter your score:"))
 
 if(90<=a<=100):
@@ -12,3 +13,22 @@ elif(50<=a<60):
     print("Your grade is D.")
 else:
     print("Your grade is F.")
+
+
+# 2.
+sub1=int(input("Enter the marks of subject1: "))
+p1= (sub1/100)*100
+sub2=int(input("Enter the marks of subject2: "))
+p2= (sub2/100)*100
+sub3=int(input("Enter the marks of subject3: "))
+p3= (sub3/100)*100
+
+
+total= sub1+sub2+sub3
+percentage=float((total/300)*100)
+
+if(p1>=33 and p2>=33 and p3>=33 and percentage>=40):
+   print(f"Your percentage is {percentage:.2f}%,you have passed!") 
+
+else:
+  print("You have failed")
