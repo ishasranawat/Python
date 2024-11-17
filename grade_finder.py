80a04addb68744807d99d14179865fd4013d8abc
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-17 10:46:57 +0530

    Create grade_finder.py

---
+++ grade_finder.py
@@ -0,0 +1,14 @@
+a= int(input("Enter your score:"))
+
+if(90<=a<=100):
+    print("Your grade is Ex.")
+elif(80<=a<90):
+    print("Your grade is A.")
+elif(70<=a<80):
+        print("Your grade is B.")
+elif(60<=a<70):
+     print("Your grade is C.")
+elif(50<=a<60):
+    print("Your grade is D.")
+else:
+    print("Your grade is F.")
