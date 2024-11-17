28278945283a2c2ae8e2fa6af67b5e997852d436
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-17 10:57:30 +0530

    Update grade_finder.py

--- grade_finder.py
+++ grade_finder.py
@@ -32,3 +32,22 @@ if(p1>=33 and p2>=33 and p3>=33 and percentage>=40):
 
 else:
   print("You have failed")
+
+
+
+# 3.
+num1= int(input("Enter num1: "))
+num2= int(input("Enter num2: "))
+num3= int(input("Enter num3: "))
+num4= int(input("Enter num4: "))
+
+if(num1>=num2 and num1>=num3 and num1>=num4):
+    print("num1 is the greatest")
+
+elif(num2>=num1 and num2>=num3 and num2>=num4):
+    print("num2 is the greatest")
+
+elif(num3>=num1 and num3>=num2 and num3>=num4):
+    print("num3 is the greatest")
+
+else:
