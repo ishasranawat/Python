5225ce77d9014a1788e02a3c39979e1e764431a7
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-18 15:39:00 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -5,4 +5,15 @@ for i in range(1, n+1):
     print("*" *(2*i-1))
 
 
+# searching  a number 
+
+tuple=(1,4,9,16,25,36,49,64,81,100)
+x=16
+index=0
+for i in tuple:
+    if (i==x):
+      print(" number found at index", index)
+      break
+    index+=1
+
 
