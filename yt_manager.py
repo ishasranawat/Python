900e6a15d1010460d219fdcc101dcb57c3b50c00
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-19 15:36:35 +0530

    Create yt_manager.py

---
+++ yt_manager.py
@@ -0,0 +1,14 @@
+import json
+
+def load_data():
+    try:
+        with open('youtube.txt', 'r') as file:
+            test = json.load(file)
+            # print(type(test))
+            return test
+    except FileNotFoundError:
+        return []
+    
+def save_data_helper(videos):
+    with open('youtube.txt', 'w') as file:
+        json.dump(videos, file)
