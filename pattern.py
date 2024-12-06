973269f332886199afcf78fafcaad2d5f818daa8
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-12-07 00:05:47 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -3,6 +3,18 @@ n=int(input("Enter the number: "))
 for i in range(1, n+1):
     print(" " * (n-i),end="")
     print("*" *(2*i-1))
+    
+
+# another method
+def pattern(n):
+    for i in range(1,n+1):
+        spaces= " " *(n-i)
+        stars= "*"* (2*i-1)
+
+        print (spaces+stars)
+
+n=int(input())
+pattern(n)
 
 
 
