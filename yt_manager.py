0baafad229d024bf5af2f413bd9317d2d6cf7447
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-12-09 11:05:01 +0530

    Update yt_manager.py

--- yt_manager.py
+++ yt_manager.py
@@ -48,3 +48,31 @@ def delete_video(videos):
     else:
         print("Invalid video index selected")
 
+
+def main():
+    videos = load_data()
+    while True:
+        print("\n Welcome to Youtube Manager | choose an option ")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice: ")
+        # print(videos)
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+            case _:
+                print("Invalid Choice")
+
+
