6831ed517a2ed851400f3cebeb1fd65b99f7725f
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-01-23 19:04:17 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -3,7 +3,8 @@ n=int(input("Enter the number: "))
 for i in range(1, n+1):
     print(" " * (n-i),end="")
     print("*" *(2*i-1))
-    
+
+
 # another method
 def pattern(n):
     for i in range(1,n+1):
