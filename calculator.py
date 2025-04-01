055d51a39f2e05000453ccf497fa616eb70b59fa
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-04-01 22:33:11 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,3 @@
-def add(n1, n2):
-    return n1 + n2
-
-def subtract(n1, n2):
-    return n1 - n2
-
-
-def multiply(n1, n2):
-    return n1 * n2
-
-
-def divide(n1, n2):
-    return n1 / n2
 
 
 operations = {
